5ae727f5c7a9d903832724f3efedb93879bd9d90
Teguh Hofstee <teguhhofstee@gmail.com>
2019-10-21 22:48:22 -0700

    more debugging

--- run.sh
+++ run.sh
@@ -5,6 +5,13 @@ apk add --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing emscripte
 mkdir build && cd build
 pwd
 
+ls /usr/bin/
+ls /usr/share/
+ls /usr/share/emscripten
+ls /usr/share/emscripten/cmake
+ls /usr/share/emscripten/cmake/Modules
+ls /usr/share/emscripten/cmake/Modules/Platform
+
 emcmake cmake ..
 make
 cd -
