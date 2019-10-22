7c6fe7a6523680c05792c7a442c4ff853685c3e1
hofstee <hofstee@users.noreply.github.com>
2019-10-22 08:34:19 +0000

    Automated deployment: Tue Oct 22 08:34:19 UTC 2019 864d624349af648787dbf4d51a6398c2c9e76156

---
+++ CMakeFiles/index.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/index.dir/src/main.cc.o"
+  "index.pdb"
+  "index.html"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/index.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
