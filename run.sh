30954b30be7fcf1fe0b2b08ac90b3f9a96cc1d30
Teguh Hofstee <teguhhofstee@gmail.com>
2019-10-21 22:45:36 -0700

    debug build

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -l
 echo "Hello world!"
 
-apk add --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing emscripten
+apk add --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing emscripten make
 mkdir build && cd build
+pwd
+
 emcmake cmake ..
 make
 cd -
